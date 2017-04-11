9386a90b39b55bf3722c06613de1e713304cf23f
GaYeon Kim <victoria1559@naver.com>
2017-04-12 03:52:31 +0900

    first

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -18,8 +18,13 @@ import java.util.Map;
 /**
  * Created by tchi on 2017. 4. 1..
  */
+/*
+ * 컨트롤러를 구현 == 클라이언트의 요청을 처리할 메서드를 구현
+ * 클라이언트는 URL로 요청을 전송
+ * 요청 URL을 어떤 메서드가 처리할지 여부를 결정하는 것이 @RequestMapping
+ */
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/calc")
 @Log4j
 public class RiotApiController {
     @Autowired
@@ -31,7 +36,7 @@ public class RiotApiController {
     @Value("${riot.api.key}")
     private String riotApiKey;
 
-    @RequestMapping(value = "/summoner/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/summoner/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Summoner querySummoner(@PathVariable("name") String summonerName) throws UnsupportedEncodingException {
         final String url = riotApiEndpoint + "/summoner/by-name/" +
                 summonerName +
