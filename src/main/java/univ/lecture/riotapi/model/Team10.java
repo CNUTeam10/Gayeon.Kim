7856d94beeb308c513f85aa9ab418577b2331f50
GaYeon Kim <victoria1559@naver.com>
2017-04-13 00:55:56 +0900

    dad

--- src/main/java/univ/lecture/riotapi/model/Team10.java
+++ src/main/java/univ/lecture/riotapi/model/Team10.java
@@ -1,34 +1,29 @@
 package univ.lecture.riotapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Team10 {
-	int teamId;
-	long now;
-	double result;
-	
-	
-	public Team10(int teamId, long now, double result) {
-		super();
-		this.teamId = teamId;
-		this.now = now;
-		this.result = result;
-	}
-	public int getTeamId() {
-		return teamId;
-	}
-	public void setTeamId(int teamId) {
-		this.teamId = teamId;
-	}
-	public long getNow() {
-		return now;
-	}
-	public void setNow(long now) {
-		this.now = now;
-	}
-	public double getResult() {
-		return result;
-	}
-	public void setResult(double result) {
-		this.result = result;
-	}
-	
+    private int teamId;
+    private long now;
+    private double result;
+    
+    public void setTeamId(int teamId)
+    {
+       this.teamId = teamId;
+    }
+    
+    public void setNow(long now)
+    {
+       this.now = now;
+    }
+    
+    public void setResult(double result)
+    {
+       this.result = result;
+    }
 }
