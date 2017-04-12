0bb65226283d2cc58df2b1dbbfebe6f6d46c1064
GaYeon Kim <victoria1559@naver.com>
2017-04-13 00:11:24 +0900

    adfs

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -1,24 +1,18 @@
 package univ.lecture.riotapi.controller;
 
-import com.google.gson.JsonObject;
 import lombok.extern.log4j.Log4j;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.spring.web.json.Json;
 import univ.lecture.riotapi.Calculator;
+import univ.lecture.riotapi.model.ObjectTeam10;
 import univ.lecture.riotapi.model.Team10;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 //import java.io.UnsupportedEncodingException;
 //import java.util.ArrayList;
 //import java.util.Date;
@@ -45,41 +39,16 @@ public class RiotApiController {
     Logger log = Logger.getLogger(this.getClass());
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public @ResponseBody Team10 queryTeam10(@RequestParam(value="exp")String exp){
+    public @ResponseBody Object queryTeam10(@RequestParam(value="exp")String exp){
 
     	Calculator cal = new Calculator();
     	double result = cal.calculate(cal.postfix(exp));
     	long now = System.currentTimeMillis();
     	Team10 team10 = new Team10(10,now,result);
-    	log.debug("interrupt");
-
-    	return team10;
+    	ObjectTeam10 objectTeam10 = new ObjectTeam10();
+    	
+    	objectTeam10.setMsg("Recorded");
+    	return objectTeam10;
     }
-
     
-
-//    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-//    public @ResponseBody Map<String, Object> getJsonByMap() {
-//    	Map<String, Object> jsonObject = new HashMap<String, Object>();
-//    	Map<String, Object> jsonSubObject = null;
-//    	ArrayList<Map<String,Object>> jsonList = new ArrayList<Map<String,Object>>();
-//
-//    	jsonSubObject = new HashMap<String, Object>();
-//        jsonSubObject.put("idx", 1);
-//        jsonSubObject.put("title", "제목입니다");
-//        jsonSubObject.put("create_date", new Date());
-//        jsonList.add(jsonSubObject);
-//        //2번째 데이터
-//        jsonSubObject = new HashMap<String, Object>();
-//        jsonSubObject.put("idx", 2);
-//        jsonSubObject.put("title", "두번째제목입니다");
-//        jsonSubObject.put("create_date", new Date());
-//        jsonList.add(jsonSubObject);
-//
-//        jsonObject.put("success", true);
-//        jsonObject.put("total_count", 10);
-//        jsonObject.put("result_list", jsonList);
-//
-//        return jsonObject;
-//    }
 }
---
+++ src/main/java/univ/lecture/riotapi/model/ObjectTeam10.java
@@ -0,0 +1,15 @@
+package univ.lecture.riotapi.model;
+
+import java.util.ArrayList;
+
+public class ObjectTeam10 {
+	private String msg;
+//	private ArrayList<Team10> list;
+//	private int total_count;
+	public String Msg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
