352db498c93731136b88847ae88b40b48d73b110
GaYeon Kim <victoria1559@naver.com>
2017-04-12 14:47:36 +0900

    adf

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -12,10 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import univ.lecture.riotapi.Calculator;
 import univ.lecture.riotapi.model.Summoner;
-import org.json.JSONObject;
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
