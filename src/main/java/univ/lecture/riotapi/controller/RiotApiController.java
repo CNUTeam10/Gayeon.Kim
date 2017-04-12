4d68e1b54e11c357189d3537551b2bf90b8be582
GaYeon Kim <victoria1559@naver.com>
2017-04-12 13:11:05 +0900

    da

--- src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
+++ src/main/java/univ/lecture/riotapi/controller/RiotApiController.java
@@ -12,16 +12,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import univ.lecture.riotapi.model.Summoner;
+
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 /**
  * Created by tchi on 2017. 4. 1..
- */
-/*
- * 컨트롤러를 구현 == 클라이언트의 요청을 처리할 메서드를 구현
- * 클라이언트는 URL로 요청을 전송
- * 요청 URL을 어떤 메서드가 처리할지 여부를 결정하는 것이 @RequestMapping
  */
 @RestController
 @RequestMapping("/api/v1/calc")
@@ -36,12 +32,12 @@ public class RiotApiController {
     @Value("${riot.api.key}")
     private String riotApiKey;
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Summoner querySummoner(@RequestBody@PathVariable("name") String summonerName) throws UnsupportedEncodingException {
+    @RequestMapping(value = "/summoner/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Summoner querySummoner(@RequestBody @PathVariable("name") String summonerName) throws UnsupportedEncodingException {
         final String url = riotApiEndpoint + "/summoner/by-name/" +
                 summonerName +
                 "?api_key=" +
-               riotApiKey;
+                riotApiKey;
 
         String response = restTemplate.getForObject(url, String.class);
         Map<String, Object> parsedMap = new JacksonJsonParser().parseMap(response);
